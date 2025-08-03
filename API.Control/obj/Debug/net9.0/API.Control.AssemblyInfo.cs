28b39a25f0b303e09f7bf061be58de0f9b351505
Gilmar Prust <gilmar.prust@outlook.com>
2025-08-03 17:31:19 -0300

    dcm ok

--- API.Control/obj/Debug/net9.0/API.Control.AssemblyInfo.cs
+++ API.Control/obj/Debug/net9.0/API.Control.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API.Control")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a426a4d074cac0d8f1b04bf85f85a79dca99f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ae3126386342b1ac4a3fa9a970e46424c350d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API.Control")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API.Control")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
